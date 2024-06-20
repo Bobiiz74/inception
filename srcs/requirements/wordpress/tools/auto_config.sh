54a99ba427fec48142b358e2d995b2f4c96f0347
Robin <robin.godtschalck@hotmail.fr>
2024-06-20 14:38:48 +0200

    chore: Update Dockerfiles for MariaDB, Nginx, and WordPress

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -7,10 +7,9 @@ if ! wp core is-installed --allow-root  ; then
     wp config create --dbname=wordpress --dbuser=$SQL_USER \
     --dbpass=$SQL_PASSWORD --dbhost=mariadb \
     --allow-root --force
-    wp core install --url="ppotier.42.fr" --title="Inception" \
+    wp core install --url="rgodtsch.42.fr" --title="Inception" \
     --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS \
-    --admin_email=ppotier@42lausanne.ch --allow-root
-	echo "MAIMAMAIMIAL"
+    --admin_email=rgodtsch@42lausanne.ch --allow-root
     wp user create $WP_USER $WP_EMAIL --user_pass=$WP_USER_PASS --allow-root
     wp config shuffle-salts --allow-root
 	# wp post create /tmp/article.txt --post_title="Inception" --post-status=publish --post_author="chatgepeto" --allow-root
