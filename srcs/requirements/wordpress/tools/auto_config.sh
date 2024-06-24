d752fa7840c58bab79c82355d7df72ea8920be62
Robin <robin.godtschalck@hotmail.fr>
2024-06-24 15:37:10 +0200

    chore: Update Dockerfiles for MariaDB, Nginx, and WordPress

--- srcs/requirements/wordpress/tools/auto_config.sh
+++ srcs/requirements/wordpress/tools/auto_config.sh
@@ -12,8 +12,8 @@ if ! wp core is-installed --allow-root ; then
     wp core install --url="$DOMAIN_NAME" --title="Inception" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_EMAIL --allow-root
     wp user create $WP_USER $WP_EMAIL --user_pass=$WP_USER_PASS --allow-root
     wp config shuffle-salts --allow-root
-    wp post create /tmp/inception.txt --post_title="Best quotes from Inception" --post_content="Here is the best quotes from Inception." --post_status=publish --post_author="Inception God"  --allow-root
-    wp theme install astra --activate --allow-root
+    wp post create /tmp/inception.txt --post_title="Best quotes from Inception" --post_content="Here is the best quotes from Inception." --post_status=publish --post_author=Bob  --allow-root
+    #wp theme install astra --activate --allow-root
     echo "Wordpress's installation complete"
 fi
 
